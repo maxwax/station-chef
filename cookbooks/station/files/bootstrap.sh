266f12e624bed1bd2221d19587bdce38e0c2a327
Maxwell Spangler <maxwell@maxwellspangler.com>
2019-12-20 23:38:35 -0700

    Remove 'git clone station' from bootstrap, we'll do that manually

--- cookbooks/station/files/bootstrap.sh
+++ cookbooks/station/files/bootstrap.sh
@@ -6,8 +6,6 @@ cd $HOME/Downloads
 
 sudo dnf install ./chef-workstation*rpm
 
-git clone https://github.com/maxwax/station
-
 sudo knife node add run_list $(hostname -f) 'recipe[station]'
 
 sudo chef-client -z
