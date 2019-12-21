6d9ac58e6202fd69b25fd49b5abd8229a82233c5
Maxwell Spangler <maxwell@maxwellspangler.com>
2019-12-20 23:48:28 -0700

    Launch firefox to Chef download page and pause for user to manually download lastest Chef workstation. Hacky, but..

--- cookbooks/station/files/bootstrap.sh
+++ cookbooks/station/files/bootstrap.sh
@@ -4,6 +4,14 @@ export EDITOR=vi
 
 cd $HOME/Downloads
 
+echo
+firefox --new-window https://downloads.chef.io/chef-workstation &
+echo
+
+echo
+read -p "Type 'go' when you've downloaded Chef Workstation to $HOME/Downloads"
+echo
+
 sudo dnf install ./chef-workstation*rpm
 
 sudo knife node add run_list $(hostname -f) 'recipe[station]'
