d613eca8c9974336550f60bf862041814d7266a3
Maxwell Spangler <maxwell@maxwellspangler.com>
2019-12-21 00:34:24 -0700

    Reworked based on testing; Added comments; Added firefox tab to virtualbox

--- cookbooks/station/files/bootstrap.sh
+++ cookbooks/station/files/bootstrap.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-export EDITOR=vi
+#
+# bootstrap.sh from https://github.com/maxwax/station
+#
+# Install Chef Workstation tool then use the 'station' Chef cookbook
+# to automate provisioning of my personal workstation
+#
+# 2019-12-20 Initial WIP
 
 cd $HOME/Downloads
+
+# I'd like to automate this, but it needs a "Download <last stable version"
+# link on Chef's download page in order to do so.  Until then, take the user
+# to this page and have them select it manually
 
 echo
 firefox --new-window https://downloads.chef.io/chef-workstation &
 echo
 
 echo
-read -p "Type 'go' when you've downloaded Chef Workstation to $HOME/Downloads"
+echo "Manually download the latest Chef Workstation via Firefox"
+echo
+read -p "Type 'go' when ready to proceed:"
 echo
 
-sudo dnf install ./chef-workstation*rpm
+sudo dnf -y install ./chef-workstation*rpm
 
-sudo knife node add run_list $(hostname -f) 'recipe[station]'
-
+# First run creates the chef-zero node object
 sudo chef-client -z
+
+# Now we can add a runlist to the node object
+sudo knife node run_list add $(hostname -f) 'recipe[station]' -z
+
+# Now run chef-client again with the station run list
+sudo chef-client -z
+
+# Launch firefox and prompt to download Extension Pack
+firefox --new-tab https://www.virtualbox.org/wiki/Downloads &
