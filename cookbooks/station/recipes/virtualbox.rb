9dad101cbda59dc06ed263dab59640311a5312d8
Maxwell Spangler <maxwell@maxwellspangler.com>
2019-12-22 22:37:51 -0700

    Attempting to use yum_repository to setup repo for Oracle VirtualBox.

--- cookbooks/station/recipes/virtualbox.rb
+++ cookbooks/station/recipes/virtualbox.rb
@@ -12,23 +12,30 @@ Install the VirtualBox hypervisor
 
 my = node['station']['user']
 
-remote_file "virtualbox-fedora-repo" do
-  path "/etc/yum.repos.d/virtualbox.repo"
-  source node['station']['virtualbox']['repo_source']
+# remote_file "virtualbox-fedora-repo" do
+#   path "/etc/yum.repos.d/virtualbox.repo"
+#   source node['station']['virtualbox']['repo_source']
+#   action :create
+#
+#   not_if { File.exists?("/etc/yum.repos.d/virtualbox.repo")}
+# end
+
+yum_repository "virtualbox" do
+  description "Oracle VirtualBox Fedora Repo"
+  baseurl "baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/$releasever/$basearch"
+  gpgkey "gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc"
   action :create
-
-  not_if { File.exists?("/etc/yum.repos.d/virtualbox.repo")}
 end
 
-remote_file "oracle-signing-key" do
-  path "/home/#{my['username']}/Downloads/#{node['station']['virtualbox']['signing_key_fname']}"
-  source node['station']['virtualbox']['signing_key_source']
-  action :create
-end
-
-execute 'install-oracle-key' do
-  command "rpm --import /home/#{my['username']}/Downloads/#{node['station']['virtualbox']['signing_key_fname']}"
-end
+# remote_file "oracle-signing-key" do
+#   path "/home/#{my['username']}/Downloads/#{node['station']['virtualbox']['signing_key_fname']}"
+#   source node['station']['virtualbox']['signing_key_source']
+#   action :create
+# end
+#
+# execute 'install-oracle-key' do
+#   command "rpm --import /home/#{my['username']}/Downloads/#{node['station']['virtualbox']['signing_key_fname']}"
+# end
 
 package node['station']['virtualbox']['package_name'] do
  action :install
