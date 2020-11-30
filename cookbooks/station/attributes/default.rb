82c709489f182ef0f5317ce63f0962371c667f59
Maxwell Spangler <maxwell@maxwellspangler.com>
2020-11-29 21:51:36 -0700

    Install 'VirtualBox' package not 'VirtualBox-6.1'

--- cookbooks/station/attributes/default.rb
+++ cookbooks/station/attributes/default.rb
@@ -198,7 +198,7 @@ node.default['station']['mount_points'] = {
 
 node.default['station']['virtualbox'] = {
   "version": "6.1",
-  "package_name": "VirtualBox-6.1",
+  "package_name": "VirtualBox",
   "repo_source": "https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo",
   "source": "https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo",
   "signing_key_source": "https://www.virtualbox.org/download/oracle_vbox.asc",
