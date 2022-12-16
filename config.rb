3ce767fa875b0c15c5140b421fc2a153becff5a3
Maxwell Spangler <maxcode@maxwellspangler.com>
2022-12-15 20:53:07 -0700

    Adding local config.rb for use of chef-zero on station cookbook

---
+++ config.rb
@@ -0,0 +1,21 @@
+
+current_dir = File.dirname(__FILE__)
+
+# Enables chef-zero with this config
+local_mode		true
+
+# Output logs to screen only
+log_level                :info
+log_location             STDOUT
+
+
+cache_type               'BasicFile'
+ssl_verify_mode          :verify_none
+cache_options( :path =>  "/home/maxwell/projects/chef/knife-configs/maxwell/checksums" )
+
+cookbook_path            ["/home/maxwell/projects/chef/station/cookbooks"]
+
+cookbook_copyright "Maxwell Spangler"
+cookbook_email     "maxcode@maxwellspangler.com"
+cookbook_license   "mit"
+
