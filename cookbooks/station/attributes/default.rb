cf517637f75e883efbdf129ceda3598a30846574
Maxwell Spangler <maxcode@maxwellspangler.com>
2022-12-22 12:19:07 -0700

    Typo, missing ':', when adding new package for gstreamer E-AC3 audio support. Easy fix.

--- cookbooks/station/attributes/default.rb
+++ cookbooks/station/attributes/default.rb
@@ -1118,7 +1118,7 @@ when 'fedora_35'
         'gstreamer1-plugins-base': 'GStreamer streaming media framework base plugins (anaconda)',
         'gstreamer1-plugins-base-tools': 'Tools for GStreamer streaming media framework base plugins (fedora)',
         'gstreamer1-plugins-good-extras': 'Extra GStreamer plugins with good code and licensing (fedora)',
-        'gstreamer1-plugin-libav', 'AV codec for E-AC3 format',
+        'gstreamer1-plugin-libav': 'AV codec for E-AC3 format',
         'gstreamer1-plugins-ugly': 'GStreamer 1.0 streaming media framework ugly plug-ins (rpmfusion-free)',
         'gstreamer-ffmpeg': 'GStreamer FFmpeg-based plug-ins (rpmfusion-free-updates)',
         'gstreamer1-plugin-openh264': 'GStreamer H.264 plugin',
