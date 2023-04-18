cd559bff7d9ec31dee3e123486d4f7a56d6d777a
Maxwell Spangler <maxcode@maxwellspangler.com>
2023-04-18 14:32:10 -0600

    Will try removing unwanted packages before installing wanted packages. This will let us remove default packages we don't want that conflict with rpmfusion packages we do want.  So the process is base install, remove unwanted base packages, install third party packages.

--- cookbooks/station/recipes/packages.rb
+++ cookbooks/station/recipes/packages.rb
@@ -10,10 +10,10 @@ node['station']['install_packages'].each do |pkg_name, _pkg_doc|
   install_packages << pkg_name
 end
 
-package install_packages do
-  action :install
-end
-
 package node['station']['remove_packages'] do
   action :remove
+end
+
+package install_packages do
+  action :install
 end
