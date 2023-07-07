935933544ee4f1b57b8ed5a7dc7cea968a9bb574
Maxwell Spangler <maxcode@maxwellspangler.com>
2023-07-07 00:21:33 -0600

    Added smartcounts script to show SMART attributes found by BackBlaze to predict drive failures

--- cookbooks/station/attributes/default.rb
+++ cookbooks/station/attributes/default.rb
@@ -199,7 +199,8 @@ node.default['station']['etc_config'] = [
 node.default['station']['usr_local'] = {
   'cookbook_scripts': {
     'bin': [
-      'hot-tub-timemachine'
+      'hot-tub-timemachine',
+      'smartcounts'
     ],
     'etc': %w(
       shell-basics
@@ -247,8 +248,8 @@ node.default['station']['usr_local'] = {
     'sbin': {
       'nas': {
         'source': 'https://github.com/maxwax/nas/archive',
-        'filename': 'v1.3.0.tar.gz',
-        'version': '1.3.0',
+        'filename': 'v1.4.0.tar.gz',
+        'version': '1.4.0',
         'file_type': 'tgz',
         'install_script': 'install.sh',
         'condition_file': '/usr/local/sbin/nas'
